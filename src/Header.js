bbe03874753f83fa3f4974ac48a436b0e4c5347d
sanket_ghormode <sanghormode2@gmail.com>
2022-04-01 22:51:10 +0530

    Header, i.e. nav-bar is been created.

--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,50 @@
 import React from 'react'
-import './Header.css'
+import './Header.css';
+import SearchIcon from '@mui/icons-material/Search';
+import ShoppingBasketIcon from '@mui/icons-material/ShoppingBasket';
 
 function Header() {
   return (
     <div className='header'>
-        Header
+        <img className='header-logo' src="https://pngimg.com/uploads/amazon/amazon_PNG11.png" alt="" srcset="" />
+
+        <div className="header-search">
+          <input type="text" className="header-searchInput" />
+          <SearchIcon className="header-searchIcon" />
+        </div>
+
+        <div className="header-nav">
+          <div className="header-option">
+            <span className="header-optionLineOne">
+              Hello Guest
+            </span>
+            <span className="header-optionLineTwo">
+              Sign In
+            </span>
+          </div>
+
+          <div className="header-option">
+          <span className="header-optionLineOne">
+              Returns
+            </span>
+            <span className="header-optionLineTwo">
+              & Order
+            </span>
+          </div>
+          <div className="header-option">
+            <span className="header-optionLineOne">
+              Your
+            </span>
+            <span className="header-optionLineTwo">
+              Prime
+            </span>
+          </div>
+
+          <div className="header-optionBasket">
+            <ShoppingBasketIcon />
+            <span className="header-optionLineTwo header-basketCount">0</span>
+          </div>
+        </div>
     </div>
   )
 }
